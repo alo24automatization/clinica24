5e59989c0c2999c18ca8329160463819ac0c835c
Abdumannon <0605AbMu@gmail.com>
2024-08-27 10:20:13 +0500

    frontend: build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import { Clinica } from "./clinica/Clinica";
-import { ChakraProvider } from "@chakra-ui/react";
+import {Clinica} from "./clinica/Clinica";
+import {ChakraProvider} from "@chakra-ui/react";
 
 function App() {
-  return (
-    <ChakraProvider >
-      <Clinica />
-    </ChakraProvider>
-  )
+    return (
+        <ChakraProvider>
+            <Clinica/>
+        </ChakraProvider>
+    )
 }
 
 export default App;
